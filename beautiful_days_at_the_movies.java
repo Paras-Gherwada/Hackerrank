288f223fb8aa1a2fd39c79ee336e53432bf6fea3
Paras Gherwada <parasgherwada41@gmail.com>
2021-10-06 09:33:08 +0530

    -

---
+++ beautiful_days_at_the_movies.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'beautifulDays' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER i
+     *  2. INTEGER j
+     *  3. INTEGER k
+     */
+    static int reverse(int number) {
+        int reverse = 0;
+        while(number > 0) {
+            reverse = (reverse * 10) + (number % 10);
+            number /= 10;
+        }
+
+        return reverse;
+    }
+
+    public static int beautifulDays(int i, int j, int k) {
+        int counter = 0;
+        while(i <= j) {
+            if((Math.abs(i - reverse(i)) % k) == 0)
+                ++counter;
+            ++i;
+        }
+
+        return counter;
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int i = Integer.parseInt(firstMultipleInput[0]);
+
+        int j = Integer.parseInt(firstMultipleInput[1]);
+
+        int k = Integer.parseInt(firstMultipleInput[2]);
+
+        int result = Result.beautifulDays(i, j, k);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
